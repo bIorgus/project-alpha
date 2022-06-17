9b08a50c7b45ccb4b53ad4eceb3935f29815c5e0
Jeremiah Dyck <jeremiahdyck01@gmail.com>
2022-06-17 16:42:31 -0600

    feature 3 complete

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    members = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="projects",
+        # on_delete=models.CASCADE, wasn't specified in instructions but seems like a good idea 6/17 16:38
+    )
+
+    def __str__(self):
+        return self.name

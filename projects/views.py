aecefefa8614c33904149ed388a804add792bcfc
Jeremiah Dyck <jeremiahdyck01@gmail.com>
2022-06-23 18:59:17 -0600

    feature 14 good enough

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,10 @@
-# from django.shortcuts import render
+# from django.http import HttpResponseRedirect
+
+from django.shortcuts import redirect
+from django.urls.base import reverse_lazy, reverse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from projects.models import Project
@@ -20,3 +24,23 @@ class ProjectDetailView(LoginRequiredMixin, DetailView):
     model = Project
     template_name = "projects/detail.html"
     context_object_name = "project_detail"
+
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    template_name = "projects/create.html"
+    context_object_name = "project_create"
+    fields = ["name", "description", "members"]
+
+    def form_valid(self, form):
+        item = form.save()
+        item.owner = self.request.user
+        item.save()
+        return redirect("home")
+
+    # should redirect to detail for newly created project,
+    # moving on for now 6/23 18:35
+
+    # def get_success_url(self, request):
+    #     # return reverse_lazy("show_project", pk=self.kwargs["pk"])
+    #     return reverse("show_project", args=request.pk)

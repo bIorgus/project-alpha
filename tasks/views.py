8bcd8a5a46841698d6f518e3b82e90b187ad8ce3
Jeremiah Dyck <jeremiahdyck01@gmail.com>
2022-06-24 12:12:47 -0600

    feature 15 good enough

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from tasks.models import Task
+
 
 # Create your views here.
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    template = "tasks/task_form.html"
+    context_object_name = "task_create"
+    fields = ["name", "start_date", "due_date", "project", "assignee"]
+
+    def form_valid(self, form):
+        item = form.save()
+        item.owner = self.request.user
+        item.save()
+        return redirect("home")
+
+    # def get_success_url(self):
+    #     return redirect("home")
+
+    # this should redirect to the detail view of the project for which
+    # the task was created, but i can't figure that out quickly enough
+    # so it will redirect to home for now
